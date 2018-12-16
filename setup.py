f84a081fdee1c2f9d322971d30e8e265d42c40cf
novomotus <zwest@novomotus.net>
2018-12-16 23:22:37 +0000

    fixed issue in setup.py file.

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     description='Makes and handles http requests using proxy servers.',
     long_description=open('README.md').read(),
     install_requires=[
-        'certifi==2018.11.29'
-        'chardet==3.0.4'
-        'idna==2.8'
-        'lxml==4.2.5'
-        'requests==2.21.0'
+        'certifi==2018.11.29',
+        'chardet==3.0.4',
+        'idna==2.8',
+        'lxml==4.2.5',
+        'requests==2.21.0',
         'urllib3==1.24.1'
     ],
     url='https://github.com/NOVOMOTUS/proxy',

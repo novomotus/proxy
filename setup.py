0670f24b85fb604563e36dfc97cd4de40dcf0fe4
novomotus <zwest@novomotus.net>
2018-12-16 23:11:40 +0000

    Adding setup.py support.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='proxy',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Makes and handles http requests using proxy servers.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'certifi==2018.11.29'
+        'chardet==3.0.4'
+        'idna==2.8'
+        'lxml==4.2.5'
+        'requests==2.21.0'
+        'urllib3==1.24.1'
+    ],
+    url='https://github.com/NOVOMOTUS/proxy',
+    author='Novomotus',
+    author_email='info@novomotus.com'
+)
